07e691931bd6df5d4bdc063539a2286bcda01306
choihooo <hochoi8621@naver.com>
2025-11-07 13:08:14 +0900

    feat(main): 실시간 부분 위젯 버튼 및 배경 추가

--- src/renderer/src/pages/Main/components/MiniRunningPanel.tsx
+++ src/renderer/src/pages/Main/components/MiniRunningPanel.tsx
@@ -1,26 +1,28 @@
+import { useMemo } from 'react';
 import { usePostureStore } from '../../../store/usePostureStore';
+import { Button } from '../../../components/Button/Button';
+import WidgetIcon from "@assets/widget.svg?react"
+import backgroundImage from "@assets/background.svg"
 
 const MiniRunningPanel = () => {
   const statusText = usePostureStore((state) => state.statusText);
 
-  const badgeClass =
-    statusText === '거북목'
-      ? 'bg-error-50 text-error-600'
-      : statusText === '정상'
-        ? 'bg-success-50 text-success-600'
-        : 'bg-grey-50 text-grey-600';
+  const runningStatus = useMemo(() => {
+    return statusText === '거북목' ? '엉금엉금 가는중..' : '씽씽 가는 중!';
+  }, [statusText]);
 
   return (
-    <div className="border-grey-100 rounded-2xl border bg-white p-4">
-      <div className="mb-3 flex items-center justify-between">
-        <p className="text-body-lg-medium text-grey-600">업급업급 가는중..</p>
-        <span
-          className={`${badgeClass} text-caption-md-medium rounded-full px-3 py-1`}
-        >
-          {statusText}
-        </span>
+    <div className="">
+      <div className="flex items-center justify-between mb-4">
+        <p className="text-caption-sm-medium text-grey-400">{runningStatus}</p>
+        <Button size="xs" variant="sub" text={<div className="flex items-center gap-2 text-yellow-500"><WidgetIcon className="w-[18px] h-[18px]" />위젯</div>} />
       </div>
-      <div className="bg-grey-100 h-[360px] w-full rounded-xl" />
+
+
+      <div
+        className="h-[421px] w-full rounded-xl bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      />
     </div>
   );
 };

6fcfa1f7daab9f4786c97b09b2e32d954556819d
Lee hwan seuk <sioo1014@naver.com>
2025-11-16 16:51:57 +0900

    fix(distance): TotalDistancePannel 컴포넌트

--- src/renderer/src/pages/Main/MainPage.tsx
+++ src/renderer/src/pages/Main/MainPage.tsx
@@ -16,8 +16,7 @@ import MainHeader from './components/MainHeader';
 import MiniRunningPanel from './components/MiniRunningPanel';
 import PosePatternPanel from './components/PosePatternPanel';
 import WebcamPanel from './components/WebcamPanel';
-import { PannelHeader } from '@ui/PannelHeader/PannelHeader';
-import AchivementMedal from '../../assets/main/achivement_meadl.svg?react';
+import TotalDistancePanel from './components/TotalDistancePanel';
 
 const LOCAL_STORAGE_KEY = 'calibration_result_v1';
 
@@ -175,39 +174,9 @@ const MainPage = () => {
                     {/* 하단 부분 */}
                     <div className="flex min-h-max flex-1 items-stretch gap-4">
                       <div className="@container flex min-h-0 w-full min-w-[552px] flex-1 flex-col items-start gap-4 self-stretch">
+                        {/*레벨 및 이동거리 section */}
                         <div className="bg-grey-0 relative h-[170px] w-full shrink-0 rounded-3xl py-5 pr-4 pl-2">
-                          <div className="flex flex-col pl-3">
-                            <PannelHeader>LV.2 거부기까지</PannelHeader>
-                            <p className="flex items-center gap-2">
-                              <span className="text-title-4xl-bold text-grey-700">
-                                400
-                              </span>
-                              <span className="text-body-lg-meidum text-grey-500">
-                                / 1,200m
-                              </span>
-                            </p>
-                            {/*게이지 바*/}
-                            <div className="bg-grey-50 relative my-[13.5px] h-3 w-[calc(100%-16px)] items-center rounded-full">
-                              <div className="bg-grey-100 absolute top-[2px] left-1/3 z-0 h-2 w-2 -translate-x-1/3 rounded-full"></div>
-                              <div className="bg-grey-100 absolute top-[2px] left-2/3 z-0 h-2 w-2 -translate-x-2/3 rounded-full"></div>
-                              {/*진행바 */}
-                              <div
-                                className="relative z-10 flex h-full items-center justify-end rounded-full bg-yellow-400 py-[3px] pr-[3px] transition-all duration-1000"
-                                style={{ width: '66.7%' }}
-                              >
-                                <div className="h-2 w-2 rounded-full bg-yellow-100 opacity-100" />
-                              </div>
-                              <AchivementMedal className="absolute top-1/2 right-[-16px] z-10000 -translate-y-1/2" />
-                            </div>
-                            <div className="text-caption-xs-regular text-grey-300 flex w-full items-center justify-between">
-                              {Array.from(
-                                { length: 4 },
-                                (_, i) => i * (1200 / 3),
-                              ).map((value) => (
-                                <span key={value}>{value}</span>
-                              ))}
-                            </div>
-                          </div>
+                          <TotalDistancePanel />
                         </div>
 
                         <div className="grid min-h-0 w-full flex-1 grid-cols-1 gap-4 @[562px]:grid-cols-2">
